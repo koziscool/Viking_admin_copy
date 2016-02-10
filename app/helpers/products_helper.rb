8cb0a4723e5fc1e4a834e9829b755e1859f951b5
koziscool <kozjob@yahoo.com>
2016-02-10 13:19:37 -0600

    first cut user face products page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,20 @@
 module ProductsHelper
+
+
+  def procucts_for_category(c)
+    if c.nil? || c['category_id'] == ""
+      Product.all
+    else
+      Product.all.where("category_id = #{c['category_id']}" )
+    end
+  end
+
+  def category_name(c)
+    if c.nil? || c['category_id'].empty?
+      "All"
+    else
+      "#{Category.find(c['category_id']).name}"
+    end
+  end
+
 end
